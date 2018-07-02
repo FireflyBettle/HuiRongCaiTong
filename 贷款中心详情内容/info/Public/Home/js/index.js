cce688f4261832c4c1112ef5ab5f3e8cb2b45181
unknown <you@example.com>
2018-07-02 09:51:20 +0800

    07.02

--- 贷款中心详情内容/info/Public/Home/js/index.js
+++ 贷款中心详情内容/info/Public/Home/js/index.js
@@ -1,13 +1,21 @@
 $('#lijibanli').click(function () {
     checkPhone();
 })
-function checkPhone(){
+function checkPhone(e){
+    var e=e || event;
     var phone =$('#phone').val();
     var name =$('#name').val();
-    if (phone=='' || name=='') {
-        alert('请输入!');
-    }else if(!(/^1[34578]\d{9}$/.test(phone))){
+    if (name=='' && phone=='') {
+        alert('请填写姓名、手机号');
+        e.preventDefault();
+    }else if (name=='') {
+        alert('请填写姓名');
+        e.preventDefault();
+    }else if (phone=='') {
+        alert('请填写手机号');
+        e.preventDefault();
+    } else if (!(/^1[34578]\d{9}$/.test(phone))) {
         alert("手机号码有误，请重写");
-        return false;
+        e.preventDefault();
     }
 }
